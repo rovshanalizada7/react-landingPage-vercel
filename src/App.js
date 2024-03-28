444fcbc8a3d653de597a0975794fe88b8dea97fe
Rovshan Alizada <rovshanalizada@gamil.com>
2024-03-28 22:14:34 +0400

    All changes noted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import '@mantine/core/styles.css';
 import './App.css';
+import { IconCircleCheck,IconBasket } from '@tabler/icons-react';
+import {Badge , Container,Drawer,Indicator ,SimpleGrid,List,Group,ThemeIcon,rem,Input,Button } from '@mantine/core';
+import Home from "./components/Home";
+import About from "./components/About";
+import Work from "./components/Work";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <Home />
+     <About />
+     <Work />
+     <Testimonial />
+     <Footer />
     </div>
+  
+  
   );
 }
 
